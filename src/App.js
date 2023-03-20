1b017b3b004b213b18823ffd65613a733b5df316
yashkvr <yashincrypto5@gmail.com>
2023-03-20 12:54:32 +0530

    First  commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useContract, useContractRead, useContractWrite, ConnectWallet } from "@thirdweb-dev/react";
 
 function App() {
+  const { contract } = useContract("0x80986885fb6f3f2Be18f988750C1fA07bA54BFC3");
+  const { data: favNumber, isLoading } = useContractRead(contract, "favNumber")
+
+  const [number, setNumber] = useState(0);
+
+  const { mutateAsync: updateFavNumber } = useContractWrite(contract, "updateFavNumber")
+
+  const handleClick = async () => {
+    try {
+      const data = await updateFavNumber([number]);
+      console.info("contract call successs", data);
+    } catch (err) {
+      console.error("contract call failure", err);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ConnectWallet />
+      <h2>My Favourite number is: {isLoading ? <span>Loading...</span> : favNumber.toString()}</h2>
+      <input type="number" onChange={(e) => (
+        setNumber(e.target.value)
+      )
+      } />
+      <button onClick={handleClick}>Update</button>
     </div>
   );
 }
